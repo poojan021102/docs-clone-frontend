9a44fe3c26a9ba5e6daa10c164dd86cdf473e44d
poojan021102 <poojanpatel02112002@gmail.com>
2025-12-13 11:35:10 +0530

    Added improvements for editor

--- src/pages/documentPage/components/ShowEditor.tsx
+++ src/pages/documentPage/components/ShowEditor.tsx
@@ -1,177 +1,258 @@
-import { useCallback, useEffect, useState, useRef } from "react"
-import Quill from "quill"
-import "quill/dist/quill.snow.css"
+import { useCallback, useEffect, useState, useRef } from "react";
+import Quill from "quill";
+import "quill/dist/quill.snow.css";
 import { QUILL_OPTIONS } from "../../../utilities/constants";
-import "./editorStyle.css"
+import "./editorStyle.css";
 import { connectSocket } from "../../../utilities/socket";
 import { appContextType } from "../../../types/appcontextType";
 import { toast } from "react-toastify";
-import QuillCursors from 'quill-cursors';
+import QuillCursors from "quill-cursors";
 import { getAISuggestion } from "../../../utilities/api";
 import { Link } from "react-router-dom";
 import ShareDialogBox from "../../../components/ShareDialogBox";
+import { Lock } from "lucide-react";
 
-Quill.register('modules/cursors', QuillCursors);
+Quill.register("modules/cursors", QuillCursors);
 
 type Props = {
-    documentId: string,
-    appContext: appContextType,
-    documentContent: any,
-    documentTitle: string
-}
+  documentId: string;
+  appContext: appContextType;
+  documentContent: any;
+  documentTitle: string;
+};
 
-export default function ShowEditor({ documentId, appContext, documentContent, documentTitle }: Props){
-    const [socket, setSocket] = useState<any>(null);
-    const [quill, setQuill] = useState<Quill | undefined>(undefined);
-    const [cursor, setCursor] = useState(null);
-    const cursorState = useRef(null);
-    const selectedDocument = useRef({
-        documentId: "",
-        documentTitle: ""
-    })
-    const [showModal, setShowModal] = useState<boolean>(false);
-    
-    const wrapperRef = useCallback((wrapper: any) => {
-        if(wrapper === null || wrapper === undefined)return;
-        wrapper.innerHTML = "";
-        const editor = document.createElement('div');
-        wrapper.append(editor);
-        const q = new Quill(editor, {
-            modules: {
-                toolbar: QUILL_OPTIONS,
-                cursors: true
-            },
-            theme: 'snow'
-        });
-        setSocket(connectSocket());
-        setQuill(q);
-        q.setContents(documentContent);
-        setCursor(q.getModule("cursors"));
-        cursorState.current = {
-            isAICalled: "not_started_writing",
-            cursorIndex: q.getLength(),
-            lastMoved: new Date()
+export default function ShowEditor({
+  documentId,
+  appContext,
+  documentContent,
+  documentTitle,
+}: Props) {
+  const [socket, setSocket] = useState<any>(null);
+  const [quill, setQuill] = useState<Quill | undefined>(undefined);
+  const [cursor, setCursor] = useState(null);
+  const cursorState = useRef(null);
+  const selectedDocument = useRef({
+    documentId: "",
+    documentTitle: "",
+  });
+  const [showModal, setShowModal] = useState<boolean>(false);
+
+  const wrapperRef = useCallback((wrapper: any) => {
+    if (wrapper === null || wrapper === undefined) return;
+    wrapper.innerHTML = "";
+    const editor = document.createElement("div");
+    wrapper.append(editor);
+    const q = new Quill(editor, {
+      modules: {
+        toolbar: QUILL_OPTIONS,
+        cursors: true,
+      },
+      theme: "snow",
+    });
+    setSocket(connectSocket());
+    setQuill(q);
+    q.setContents(documentContent);
+    setCursor(q.getModule("cursors"));
+    cursorState.current = {
+      isAICalled: "not_started_writing",
+      cursorIndex: q.getLength(),
+      lastMoved: new Date(),
+    };
+  }, []);
+
+  const handleNewUserConnectionServerToClient = (args) => {
+    toast(
+      `${args.user.firstName + args.user.lastName} (${args.user.email}) joined`
+    );
+  };
+
+  const handleDisconnectingServerToClient = (args) => {
+    toast(
+      `${args.user.firstName + args.user.lastName} (${
+        args.user.email
+      }) disconnected`
+    );
+    cursor.removeCursor(args.socketId);
+  };
+
+  const handleEditorSelectionChange = (range, _, source) => {
+    socket.emit("editorSelectionChangeClientToServer", {
+      range,
+      user: appContext.user,
+      documentId,
+    });
+  };
+
+  const handleEditorOnTextChange = (delta, _, source) => {
+    if (source == "api") return;
+    // aiSuggestion.current = "";
+
+    cursorState.current = {
+      cursorIndex: quill?.getSelection().index,
+      lastMoved: new Date(),
+      isAICalled: "not_called",
+    };
+    socket.emit("textChangeClientToServer", {
+      delta,
+      documentId,
+      content: quill?.getContents(),
+    });
+  };
+
+  const colors = ["blue", "red", "orange", "green"];
+
+  function pickRandomString(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+  }
+
+  const handleEditorSelectionChangeServerToClient = (args) => {
+    cursor.createCursor(
+      args.socketId,
+      args.user.firstName + args.user.lastName,
+      pickRandomString(colors)
+    );
+    cursor.moveCursor(args.socketId, args.range); // <== cursor data from previous step
+    cursor.toggleFlag(args.socketId, true);
+  };
+
+  const handleTextChangeServerToClient = (args) => {
+    quill?.updateContents(args.delta);
+  };
+
+  useEffect(() => {
+    if (!quill || !socket || !appContext || !appContext.user) return;
+
+    socket.emit("userConnectedClientToServer", {
+      documentId,
+      user: appContext.user,
+    });
+    socket.on(
+      "userConnectedServerToClient",
+      handleNewUserConnectionServerToClient
+    );
+    socket.on("disconnectingServerToClient", handleDisconnectingServerToClient);
+    quill.on("text-change", handleEditorOnTextChange);
+    socket.on("textChangeServerToClient", handleTextChangeServerToClient);
+    quill.on("selection-change", handleEditorSelectionChange);
+    socket.on(
+      "editorSelectionChangeServerToClient",
+      handleEditorSelectionChangeServerToClient
+    );
+    const interval = setInterval(async () => {
+      const currentDate = new Date();
+      const previousDate = cursorState.current.lastMoved;
+      if (
+        cursorState.current &&
+        quill &&
+        quill.getLength() - 1 &&
+        currentDate.getTime() - cursorState.current.lastMoved.getTime() >=
+          3000 &&
+        cursorState.current.isAICalled == "not_called"
+      ) {
+        cursorState.current.isAICalled = "in_progress";
+
+        const response = await getAISuggestion(
+          quill.getText().substring(0, cursorState.current.cursorIndex)
+        );
+        // const response = {
+        //     status: true,
+        //     response: "AI Response"
+        // }
+        if (response.status && previousDate == cursorState.current.lastMoved) {
+          if ("response" in response && response.response) {
+            quill.insertText(
+              cursorState.current.cursorIndex,
+              response.response,
+              "user"
+            );
+            quill.setSelection(
+              cursorState.current.cursorIndex + response.response.length
+            );
+          }
         }
-    }, []);
 
-    const handleNewUserConnectionServerToClient = args => {
-        toast(`${args.user.firstName + args.user.lastName} (${args.user.email}) joined`);
-    }
-    
-    const handleDisconnectingServerToClient = args => {
-        toast(`${args.user.firstName + args.user.lastName} (${args.user.email}) disconnected`);
-        cursor.removeCursor(args.socketId)
-    }
+        cursorState.current.isAICalled = "called";
+      }
+    }, 2000);
 
-    const handleEditorSelectionChange = (range, _, source) => {
-        socket.emit("editorSelectionChangeClientToServer", {
-            range, user: appContext.user, documentId
-        });
-    }
+    socket.on("disconnect", () => {
+      socket.emit("disconnectingClientToServer", {
+        documentId,
+        user: appContext.user,
+      });
+    });
 
-    const handleEditorOnTextChange = (delta, _, source) => {
-        if(source == 'api')return;
-        // aiSuggestion.current = "";
-        
-        cursorState.current = {
-            cursorIndex: quill?.getSelection().index,
-            lastMoved: new Date(),
-            isAICalled: "not_called"
-        }
-        socket.emit('textChangeClientToServer', {
-            delta,
-            documentId,
-            content: quill?.getContents()
-        })   
-    }
+    socket.on("disconnectingClientToServer", handleDisconnectingServerToClient);
 
-    const handleEditorSelectionChangeServerToClient = args=>{
-        cursor.createCursor(args.socketId, args.user.firstName + args.user.lastName, args.user.color ? args.user.color : 'black');
-        cursor.moveCursor(args.socketId, args.range); // <== cursor data from previous step
-        cursor.toggleFlag(args.socketId, true);
-    }
+    return () => {
+      socket.emit("disconnectingClientToServer", {
+        documentId,
+        user: appContext.user,
+      });
+      // socket.off("disconnect", handleDisconnectingServerToClient);
+      socket.on(
+        "disconnectingClientToServer",
+        handleDisconnectingServerToClient
+      );
 
-    const handleTextChangeServerToClient = args => {
-        quill?.updateContents(args.delta);
-    }
+      socket.off(
+        "userConnectedServerToClient",
+        handleNewUserConnectionServerToClient
+      );
+      socket.off("textChangeServerToClient", handleTextChangeServerToClient);
+      quill.off("text-change", handleEditorOnTextChange);
+      socket.off(
+        "editorSelectionChangeServerToClient",
+        handleEditorSelectionChangeServerToClient
+      );
+      clearInterval(interval);
+      socket.disconnect();
+    };
+  }, [socket, quill]);
 
-    useEffect(() => {
-        if(!quill || !socket || !appContext || !appContext.user)return;
+  const handleShareDocumentClick = () => {
+    setShowModal(true);
+    selectedDocument.current.documentId = documentId;
+    selectedDocument.current.documentTitle = documentTitle;
+  };
 
-        socket.emit("userConnectedClientToServer", {
-            documentId, 
-            user: appContext.user
-        })
-        socket.on("userConnectedServerToClient", handleNewUserConnectionServerToClient);
-        socket.on("disconnectingServerToClient", handleDisconnectingServerToClient);
-        quill.on("text-change", handleEditorOnTextChange)
-        socket.on("textChangeServerToClient", handleTextChangeServerToClient)
-        quill.on("selection-change", handleEditorSelectionChange)
-        socket.on("editorSelectionChangeServerToClient", handleEditorSelectionChangeServerToClient);
-        const interval = setInterval(async()=> {
-            const currentDate = new Date();
-            const previousDate = cursorState.current.lastMoved;
-            if(cursorState.current && quill && quill.getLength() - 1 && currentDate.getTime() - cursorState.current.lastMoved.getTime() >= 3000  && cursorState.current.isAICalled == "not_called"){
-               
-                cursorState.current.isAICalled = "in_progress";
-               
-                const response = await getAISuggestion(quill.getText().substring(0, cursorState.current.cursorIndex));
-                // const response = {
-                //     status: true,
-                //     response: "AI Response"
-                // }
-                if(response.status && previousDate == cursorState.current.lastMoved){
-                    if('response' in response && response.response){
-                        quill.insertText(cursorState.current.cursorIndex, response.response, "user");
-                        quill.setSelection(cursorState.current.cursorIndex + response.response.length)
-                    }
-                }
+  return (
+    <div className="flex flex-col min-h-screen">
+      {showModal && (
+        <ShareDialogBox
+          selectedDocument={selectedDocument}
+          setShowModal={setShowModal}
+        />
+      )}
 
-                cursorState.current.isAICalled = "called"
-            }
-        }, 2000)
-        
-        return () => {
-            socket.emit("disconnectingClientToServer", {
-                documentId,
-                user: appContext.user
-            })
-            socket.on("disconnectingServerToClient", handleDisconnectingServerToClient);
-            socket.off("userConnectedServerToClient", handleNewUserConnectionServerToClient);
-            socket.off("textChangeServerToClient", handleTextChangeServerToClient)
-            quill.off("text-change", handleEditorOnTextChange)
-            socket.off("editorSelectionChangeServerToClient", handleEditorSelectionChangeServerToClient);
-            clearInterval(interval);
-            socket.disconnect();
-        };
-    }, [socket, quill]);
-
-    const handleShareDocumentClick = () => {
-        setShowModal(true);
-        selectedDocument.current.documentId = documentId;
-        selectedDocument.current.documentTitle = documentTitle ;
-    }
-
-    return(
-        <div className="flex justify-center items-center flex-col">
-            {
-                showModal && <ShareDialogBox selectedDocument={selectedDocument} setShowModal={setShowModal}/>
-            }
-            
-            <div className="pl-3 mb-3 mt-3 pr-5 w-[100%] flex flex-wrap items-center justify-between">
-                <div className="mb-2 text-2xl font-semibold">
-                    Document Title
-                </div>
-                <div className="flex justify-between items-center">
-                    <Link className="pr-3 underline text-slate-400" to = "/allDocuments">Home</Link>
-                    <button onClick = {() => {handleShareDocumentClick()}} className="bg-blue-300 hover:bg-blue-400 p-1 pl-5 pr-5 text-lg font-semibold rounded-3xl">Share</button>
-                </div>
-            </div>
-            <div className="flex justify-center items-center">
-                <div id="container" className="flex flex-col justify-center items-center" ref = {wrapperRef}>
-                
-                </div>
-            </div>
+      <div className="pl-3 mb-3 mt-3 pr-5 w-[100%] flex flex-wrap items-center justify-between">
+        <div className="mb-2 text-2xl font-semibold">{documentTitle}</div>
+        <div className="flex justify-between items-center">
+          <Link
+            className="pr-3 underline text-slate-400 active:scale-90 transition-transform"
+            to="/allDocuments"
+          >
+            Home
+          </Link>
+          <button
+            onClick={() => {
+              handleShareDocumentClick();
+            }}
+            className="bg-blue-300 hover:bg-blue-400 p-1 pl-5 pr-5 text-lg font-semibold rounded-3xl flex items-center active:scale-90 transition-transform"
+          >
+            <Lock size={18} color="#000000" />
+            <div>Share</div>
+          </button>
         </div>
-    )
+      </div>
+      {/* <div className="flex justify-center items-center h-full"> */}
+      <div
+        id="container"
+        className="flex flex-col items-center"
+        ref={wrapperRef}
+      ></div>
+      {/* </div> */}
+    </div>
+  );
 }
--- src/pages/documentPage/documentPage.tsx
+++ src/pages/documentPage/documentPage.tsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom"
+import { useParams } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState, useRef } from "react";
 import useAppContext from "../../context/useAppContext";
@@ -10,69 +10,87 @@ import { toast } from "react-toastify";
 import { canAccess } from "../../utilities/api";
 
 type documentParams = {
-    documentId:string
-}
+  documentId: string;
+};
 
-export default function DocumentPage(){
-    const { documentId } = useParams<documentParams>();
-    const { getAppContext } = useAppContext();
-    const appContext = getAppContext();
-    const navigate = useNavigate();
-    const [loadingMessage, setLoadingMessage] = useState<string>("");
-    const [loading, setLoading] = useState<boolean>(true);
-    const { getCookie } = useAppCookie();
-    let documentContent = useRef("");
-    let documentTitle = useRef("");
-    const authToken = getCookie();
+export default function DocumentPage() {
+  const { documentId } = useParams<documentParams>();
+  const { getAppContext } = useAppContext();
+  const appContext = getAppContext();
+  const navigate = useNavigate();
+  const [loadingMessage, setLoadingMessage] = useState<string>("");
+  const [loading, setLoading] = useState<boolean>(true);
+  const { getCookie } = useAppCookie();
+  let documentContent = useRef("");
+  let documentTitle = useRef("");
+  const authToken = getCookie();
 
-    useEffect(() => {
-        if(appContext.loggedInStatus === "logout"){
-            navigate("/");
-        }
-        if(appContext.loggedInStatus === "inProgress"){
-            setLoading(true);
-            setLoadingMessage("Checking for user account");
-        }
-        else if(appContext.loggedInStatus === "success"){
-            const temp = async() => {
-                setLoading(true);
-                setLoadingMessage("Fetching document");
-                if(appContext.user && appContext.user.userId){
-                    
-                    if(documentId && authToken){
-                        const checkAccess = await canAccess(appContext.user.userId, appContext.user.email, documentId, authToken);
-                        if(!checkAccess.status){
-                            toast("You are not allowed to access document");
-                            navigate("/");
-                        }
-                        else{
-                           if(authToken && documentId){
-                                const document = await getDocumentContent(appContext.user?.userId, appContext.user?.email, authToken, documentId);
-                                if(document.status && 'content' in document){
-                                    documentContent.current = document.content;
-                                    documentTitle.current = document.documentTitle;
-                                }
-                                else{
-                                    if('message' in document){
-                                        toast(document.message);
-                                    }
-                                    navigate("/");
-                                }
-                            }
-                        }
-                        setLoading(false);
-                    }
+  useEffect(() => {
+    if (appContext.loggedInStatus === "logout") {
+      navigate("/");
+    }
+    if (appContext.loggedInStatus === "inProgress") {
+      setLoading(true);
+      setLoadingMessage("Checking for user account");
+    } else if (appContext.loggedInStatus === "success") {
+      const temp = async () => {
+        setLoading(true);
+        setLoadingMessage("Fetching document");
+        if (appContext.user && appContext.user.userId) {
+          if (documentId && authToken) {
+            const checkAccess = await canAccess(
+              appContext.user.userId,
+              appContext.user.email,
+              documentId,
+              authToken
+            );
+            if (!checkAccess.status) {
+              toast("You are not allowed to access document");
+              navigate("/");
+            } else {
+              if (authToken && documentId) {
+                const document = await getDocumentContent(
+                  appContext.user?.userId,
+                  appContext.user?.email,
+                  authToken,
+                  documentId
+                );
+                if (document.status && "content" in document) {
+                  documentContent.current = document.content;
+                  documentTitle.current = document.documentTitle;
+                } else {
+                  if ("message" in document) {
+                    toast(document.message);
+                  }
+                  navigate("/");
                 }
+              }
             }
-            temp();
+            setLoading(false);
+          }
         }
-    }, [appContext]);
+      };
+      temp();
+    }
+  }, [appContext]);
 
-    return(
-        <div>
-            {
-                appContext.loggedInStatus === "inProgress" || appContext.loggedInStatus === "logout" || !documentId || !appContext || !appContext.user || loading ? <Loading message={loadingMessage}/>:<ShowEditor documentTitle = {documentTitle.current} documentContent = {documentContent.current} appContext = {appContext} documentId={documentId}/>
-            }           
-        </div>
-    )
+  return (
+    <div className="min-h-screen">
+      {appContext.loggedInStatus === "inProgress" ||
+      appContext.loggedInStatus === "logout" ||
+      !documentId ||
+      !appContext ||
+      !appContext.user ||
+      loading ? (
+        <Loading message={loadingMessage} />
+      ) : (
+        <ShowEditor
+          documentTitle={documentTitle.current}
+          documentContent={documentContent.current}
+          appContext={appContext}
+          documentId={documentId}
+        />
+      )}
+    </div>
+  );
 }
