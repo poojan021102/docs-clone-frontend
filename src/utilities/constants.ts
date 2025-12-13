6dcd8ad4ede5959b86e3601cd58745aaa36a15ba
poojan021102 <94627901+poojan021102@users.noreply.github.com>
2025-12-13 18:49:12 +0530

    Added improvements for editor (#1)
    * Added improvements for editor
    
    * Resolved typescript related issues

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,5 +1,5 @@
-export const BACKEND_BASE_URI: string = "https://docs-clone-backend-adx5.onrender.com"
-export const AI_SUGGESTION_BASE_URI: string = "https://docs-clone-generative-ai.onrender.com"
+export const BACKEND_BASE_URI: string = "http://localhost:3000";
+export const AI_SUGGESTION_BASE_URI: string = "http://localhost:3002";
 // Auth
 export const BACKEND_AUTH_URI: string = BACKEND_BASE_URI + "/auth";
 export const LOGIN_URI: string = BACKEND_AUTH_URI + "/login";
@@ -18,14 +18,18 @@ export const BACKEND_DOCUMENT_URI: string = BACKEND_BASE_URI + "/document";
 
 // Document
 export const CREATE_DOCUMENT_URI: string = BACKEND_DOCUMENT_URI + "/create";
-export const ALL_DOCUMENTS_URI: string = BACKEND_DOCUMENT_URI + "/getAllDocuments"
+export const ALL_DOCUMENTS_URI: string =
+  BACKEND_DOCUMENT_URI + "/getAllDocuments";
 export const DELETE_DOCUMENT_URI: string = BACKEND_DOCUMENT_URI + "/delete";
-export const GET_DOCUMENT_CONTENT: string = BACKEND_DOCUMENT_URI + "/getContent";
-export const GET_ALL_SHARED_EMAIL: string = BACKEND_DOCUMENT_URI + "/allSharedUserEmail";
+export const GET_DOCUMENT_CONTENT: string =
+  BACKEND_DOCUMENT_URI + "/getContent";
+export const GET_ALL_SHARED_EMAIL: string =
+  BACKEND_DOCUMENT_URI + "/allSharedUserEmail";
 export const SHARE_DOCUMENT_URL: string = BACKEND_DOCUMENT_URI + "/share";
 export const REMOVE_ACCESS_URL: string = BACKEND_DOCUMENT_URI + "/removeAccess";
 export const CAN_ACCESS_URL: string = BACKEND_DOCUMENT_URI + "/canAccess";
-export const SHARED_DOCUMENT_URL: string = BACKEND_DOCUMENT_URI + "/sharedDocuments";
+export const SHARED_DOCUMENT_URL: string =
+  BACKEND_DOCUMENT_URI + "/sharedDocuments";
 
 // AI Suggestion
 export const AI_SUGGESTION_URI: string = AI_SUGGESTION_BASE_URI + "/suggestion";
@@ -34,24 +38,24 @@ export const AI_SUGGESTION_URI: string = AI_SUGGESTION_BASE_URI + "/suggestion";
 export const LOGIN_FIRST_MESSAGE: string = "You have to login first";
 
 export const QUILL_OPTIONS = [
-    ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
-    ['blockquote', 'code-block'],
-    ['link', 'image', 'video', 'formula'],
-  
-    [{ 'header': 1 }, { 'header': 2 }, { 'header': 3 }, { 'header': 4 }],               // custom button values
-    [{ 'list': 'ordered'}, { 'list': 'bullet' }, { 'list': 'check' }],
-    [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
-    [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
-    [{ 'direction': 'rtl' }],                         // text direction
-  
-    [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
-    [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
-  
-    [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
-    [{ 'font': [] }],
-    [{ 'align': [] }],
-  
-    ['clean']                                         // remove formatting button
-  ];
+  ["bold", "italic", "underline", "strike"], // toggled buttons
+  ["blockquote", "code-block"],
+  ["link", "image", "video", "formula"],
 
-export const SOCKET_SERVER_URL = "https://docs-clone-socket.onrender.com1";
+  [{ header: 1 }, { header: 2 }, { header: 3 }, { header: 4 }], // custom button values
+  [{ list: "ordered" }, { list: "bullet" }, { list: "check" }],
+  [{ script: "sub" }, { script: "super" }], // superscript/subscript
+  [{ indent: "-1" }, { indent: "+1" }], // outdent/indent
+  [{ direction: "rtl" }], // text direction
+
+  [{ size: ["small", false, "large", "huge"] }], // custom dropdown
+  [{ header: [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ color: [] }, { background: [] }], // dropdown with defaults from theme
+  [{ font: [] }],
+  [{ align: [] }],
+
+  ["clean"], // remove formatting button
+];
+
+export const SOCKET_SERVER_URL = "http://localhost:3001";
